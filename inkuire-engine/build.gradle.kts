df4c08df5b9f26846a3c49a1cbab9bdd3f8d3b70
Kacper Korban <kacper.f.korban@gmail.com>
2020-04-24 00:21:56 +0200

    Added parser for basic kotlin signatures

--- inkuire-engine/build.gradle.kts
+++ inkuire-engine/build.gradle.kts
@@ -10,8 +10,18 @@ repositories{
 }
 
 dependencies{
-    val scalaVersion = "2.12"
+    val scalaVersion = "2.13"
 
-    implementation("org.scala-lang:scala-library:$scalaVersion.8")
+    implementation("org.scala-lang:scala-library:$scalaVersion.1")
+
+    testImplementation("junit:junit:4.13")
+    testImplementation("org.scalatest:scalatest_$scalaVersion:3.1.1")
+    testRuntimeOnly("org.scala-lang.modules:scala-xml_$scalaVersion:1.3.0")
+
+    implementation("org.scala-lang.modules:scala-parser-combinators_$scalaVersion:1.1.2")
+
+    implementation("org.typelevel:cats-core_$scalaVersion:2.1.1")
+    implementation("org.typelevel:cats-effect_$scalaVersion:2.1.1")
+    implementation("org.typelevel:cats-mtl-core_$scalaVersion:0.7.1")
+    implementation("org.typelevel:mouse_$scalaVersion:0.25")
 }
-
---
+++ inkuire-engine/src/test/scala/org/virtuslab/inkuire/engine/parser/KotlinSignatureParserTest.scala
@@ -0,0 +1,74 @@
+package org.virtuslab.inkuire.engine.parser
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.virtuslab.inkuire.engine.model.{GenericType, Signature}
+import org.virtuslab.inkuire.engine.model.Type._
+
+class KotlinSignatureParserTest extends AnyFlatSpec with Matchers {
+
+  it should "parse simple signature" in {
+    //given
+    val str = "Int.(String) -> Double"
+
+    //when
+    val res = KotlinSignatureParser.parse(str)
+
+    //then
+    val expectedRes = Right(Signature("Int".concreteType, Seq("String".concreteType), "Double".concreteType))
+
+    res should equal (expectedRes)
+  }
+
+  it should "parse signature with multiple arguments" in {
+    //given
+    val str = "Int.(String, Long, Float) -> Double"
+
+    //when
+    val res = KotlinSignatureParser.parse(str)
+
+    //then
+    val expectedRes = Right(Signature("Int".concreteType, Seq("String".concreteType, "Long".concreteType, "Float".concreteType), "Double".concreteType))
+
+    res should equal (expectedRes)
+  }
+
+  it should "parse signature without arguments" in {
+    //given
+    val str = "Int.() -> Double"
+
+    //when
+    val res = KotlinSignatureParser.parse(str)
+
+    //then
+    val expectedRes = Right(Signature("Int".concreteType, Seq(), "Double".concreteType))
+
+    res should equal (expectedRes)
+  }
+
+  it should "parse signature with generic return type" in {
+    //given
+    val str = "String.(Int) -> List<String>"
+
+    //when
+    val res = KotlinSignatureParser.parse(str)
+
+    //then
+    val expectedRes = Right(Signature("String".concreteType, Seq("Int".concreteType), GenericType("List", Seq("String".concreteType))))
+
+    res should equal (expectedRes)
+  }
+
+  it should "parse signature with generic receiver" in {
+    //given
+    val str = "Array<Double>.(Int) -> String"
+
+    //when
+    val res = KotlinSignatureParser.parse(str)
+
+    //then
+    val expectedRes = Right(Signature(GenericType("Array", Seq("Double".concreteType)), Seq("Int".concreteType), "String".concreteType))
+
+    res should equal (expectedRes)
+  }
+}
