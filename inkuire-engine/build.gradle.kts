ff4a98857a1698294eea22edb5e2b157854e1fa6
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-04-28 10:14:06 +0200

    Fix tests

--- inkuire-engine/build.gradle.kts
+++ inkuire-engine/build.gradle.kts
@@ -3,10 +3,12 @@ version = "1.0-SNAPSHOT"
 
 plugins{
     scala
+    id("com.github.maiflai.scalatest") version "0.26"
 }
 
 repositories{
     jcenter()
+    mavenCentral()
 }
 
 dependencies{
@@ -17,8 +19,8 @@ dependencies{
     testImplementation("junit:junit:4.13")
     testImplementation("org.scalatest:scalatest_$scalaVersion:3.1.1")
     testRuntimeOnly("org.scala-lang.modules:scala-xml_$scalaVersion:1.3.0")
-
     implementation("org.scala-lang.modules:scala-parser-combinators_$scalaVersion:1.1.2")
+    testRuntimeOnly("com.vladsch.flexmark:flexmark-all:0.35.10")
 
     implementation("org.typelevel:cats-core_$scalaVersion:2.1.1")
     implementation("org.typelevel:cats-effect_$scalaVersion:2.1.1")
