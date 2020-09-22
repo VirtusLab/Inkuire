21d71ec9cb568016b4d5970ce23e0ac2aaff92c8
Filip Zybała <filip.zybala@gmail.com>
2020-09-22 15:12:52 +0200

    Enchance CSS

--- inkuire-engine/src/main/scala/org/virtuslab/inkuire/engine/http/Templates.scala
+++ inkuire-engine/src/main/scala/org/virtuslab/inkuire/engine/http/Templates.scala
@@ -1,22 +1,66 @@
 package org.virtuslab.inkuire.engine.http
 
 import org.virtuslab.inkuire.model.OutputFormat
+import scalatags.Text
 import scalatags.Text.all._
+
 import collection.JavaConverters._
 
 object Templates {
+  private def logoHtml = div(id := "logo-div")(
+    img(id := "logo", src := s"/assets/logoinkuire.png")
+  )
+
+  private def resources = List(
+    link(href := "https://fonts.googleapis.com/icon?family=Material+Icons", rel := "stylesheet"),
+    link(rel := "stylesheet", href := "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"),
+    link(
+      rel := "stylesheet",
+      href := "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+    ),
+    link(
+      rel := "stylesheet",
+      href := "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+    ),
+    link(rel := "stylesheet", href := "/assets/bootstrap-theme.css"),
+    link(rel := "stylesheet", href := "/assets/styles.css")
+  )
+
   def formTemplate(): String = {
+    def generateExamples(queries: String*): Seq[Text.TypedTag[String]] = queries.zipWithIndex.map {
+      case (q, i) => example(q, s"exampleform$i")
+    }
+
+    def example(query: String, formId: String): Text.TypedTag[String] = div(cls := "form-div")(
+      form(id := formId, cls := "query-form")(method := "post")(
+        div(cls := "query-input code")(
+          input(name := "query", cls := "form-control", value := query, readonly := true)
+        ),
+        div(cls := "query-button")(
+          input(`type` := "submit", value := "Query", cls := "btn btn-primary")
+        ),
+      )
+    )
+
     html(
-      body(style := "display: flex; justify-content: center; align-items: center;")(
-        div(style := "vertical-align: middle;text-align: center; width: 600px")(
-          form(method := "post")(
-            div(
-              input(name := "query", style := "width: 100%")
+      head(
+        resources
+      ),
+      body(
+        logoHtml,
+        div(cls := "form-div")(
+          form(id := "form", cls := "query-form")(method := "post")(
+            div(cls := "query-input code")(
+              input(name := "query", cls := "form-control", placeholder := "Enter signature query")
             ),
-            div(
-              input(`type` := "submit", value := "Send")
-            )
+            div(cls := "query-button")(
+              input(`type` := "submit", value := "Send", cls := "btn btn-primary")
+            ),
           )
+        ),
+        div(id := "examples")(
+          h2("Examples"),
+          generateExamples("(String) -> Int", "Int.() -> String", "<K, V> Map<K,V>.(K) -> V?")
         )
       )
     ).toString()
@@ -24,21 +68,40 @@ object Templates {
 
   def result(results: OutputFormat): String = {
     html(
+      head(
+        resources
+      ),
       body(
+        logoHtml,
         h1(s"Result for: ${results.getQuery}"),
-        table(style := "width: 100%;")(
-          tr(
-            th(style := "text-align: left")("Name"),
-            th(style := "text-align: left")("Signature"),
-            th(style := "text-align: left")("Localization")
-          ),
-          for (res <- results.getMatches.asScala.toList)
-            yield
-              tr(
-                td(res.getFunctionName),
-                td(res.getPrettifiedSignature),
-                td(res.getLocalization)
+        div(id := "table-container")(
+          table(cls := "table table-borderless table-stripped")(
+            thead(
+              tr(cls := "result-table-header")(
+                th(cls := "left-header-cell", scoped := "col")("#"),
+                th(scoped := "col")("Name"),
+                th(scoped := "col")("Signature"),
+                th(cls := "right-header-cell", scoped := "col")("Localization")
               )
+            ),
+            tbody(
+              results.getMatches.asScala.toList.zipWithIndex.map {
+                case (res, i) =>
+                  tr(cls := "code")(
+                    th(scoped := "row")(i + 1),
+                    td(res.getFunctionName),
+                    td(res.getPrettifiedSignature),
+                    td(res.getLocalization)
+                  )
+              }
+            )
+          )
+        ),
+        div(id := "header")(
+          a(href := "./query")(
+            span(cls := "material-icons")("undo"),
+            "Go back to query input"
+          )
         )
       )
     ).toString()
