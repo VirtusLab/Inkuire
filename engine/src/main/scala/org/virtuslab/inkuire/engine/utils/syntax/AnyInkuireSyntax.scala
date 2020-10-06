fd819cceaec3d9ec5e6f1445a2c34ee247d19a02
Kacper Korban <kacper.f.korban@gmail.com>
2020-10-06 14:58:29 +0200

    Format files with scalafmt

--- engine/src/main/scala/org/virtuslab/inkuire/engine/utils/syntax/AnyInkuireSyntax.scala
+++ engine/src/main/scala/org/virtuslab/inkuire/engine/utils/syntax/AnyInkuireSyntax.scala
@@ -3,6 +3,6 @@ package org.virtuslab.inkuire.engine.utils.syntax
 trait AnyInkuireSyntax {
   implicit class AnyInkuireSyntax[A](v: A) {
     def right[L]: Either[L, A] = Right(v)
-    def left[R]: Either[A, R] = Left(v)
+    def left[R]:  Either[A, R] = Left(v)
   }
 }
