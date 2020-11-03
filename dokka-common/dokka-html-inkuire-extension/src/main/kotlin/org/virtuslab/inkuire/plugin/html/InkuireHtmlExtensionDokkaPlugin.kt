edd6c0c28a498be598f39764ae3801df1d93ad47
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-11-03 14:16:17 +0100

    Refactor DocumentableToPageTranslator to be PageTransformer

--- dokka-common/dokka-html-inkuire-extension/src/main/kotlin/org/virtuslab/inkuire/plugin/html/InkuireHtmlExtensionDokkaPlugin.kt
+++ dokka-common/dokka-html-inkuire-extension/src/main/kotlin/org/virtuslab/inkuire/plugin/html/InkuireHtmlExtensionDokkaPlugin.kt
@@ -1,22 +1,12 @@
 package org.virtuslab.inkuire.plugin.html
 
-import org.jetbrains.dokka.CoreExtensions
 import org.jetbrains.dokka.base.DokkaBase
-import org.jetbrains.dokka.base.translators.documentables.DefaultDocumentableToPageTranslator
 import org.jetbrains.dokka.plugability.DokkaPlugin
 
 class InkuireHtmlExtensionDokkaPlugin : DokkaPlugin() {
     private val dokkaBase by lazy { plugin<DokkaBase>() }
 
-    val documentableToPageTranslator by extending {
-        CoreExtensions.documentableToPageTranslator providing {
-            InkuireHtmlExtensionDocumentableToPageTranslator(
-                DefaultDocumentableToPageTranslator(
-                    it.single(dokkaBase.commentsToContentConverter),
-                    it.single(dokkaBase.signatureProvider),
-                    it.logger
-                )
-            )
-        } override dokkaBase.documentableToPageTranslator
+    val inkuireJsInstaller by extending {
+        dokkaBase.htmlPreprocessors with InkuireJsInstaller
     }
 }
---
+++ dokka-common/dokka-html-inkuire-extension/src/main/kotlin/org/virtuslab/inkuire/plugin/html/InkuireJsInstaller.kt
@@ -0,0 +1,26 @@
+package org.virtuslab.inkuire.plugin.html
+
+import org.jetbrains.dokka.model.DModule
+import org.jetbrains.dokka.model.childrenOfType
+import org.jetbrains.dokka.pages.*
+import org.jetbrains.dokka.transformers.pages.PageTransformer
+import org.virtuslab.inkuire.plugin.translators.InkuireDocumentableToPageTranslator
+
+object InkuireJsInstaller : PageTransformer {
+
+    override fun invoke(input: RootPageNode): RootPageNode {
+        val resources = listOf("scripts/inkuire.js", "styles/inkuire-styles.css", "images/inkuire-search.png")
+        val dbFiles = input.childrenOfType<ModulePageNode>().flatMap {
+            InkuireDocumentableToPageTranslator.invoke(it.documentable as DModule)
+        }
+        return input.modified(
+            children = input.children + resources.map {
+                RendererSpecificResourcePage(it, emptyList(), RenderingStrategy.Copy("/inkuire/$it"))
+            } + dbFiles
+        ).transformContentPagesTree {
+            it.modified(
+                embeddedResources = it.embeddedResources + resources
+            )
+        }
+    }
+}
