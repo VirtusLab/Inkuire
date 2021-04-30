11d357dd06f231e67272fe3ecf97cac53decfe11
Kacper Korban <kacper.f.korban@gmail.com>
2021-04-30 18:07:17 +0200

    Add examples

--- engineHttp/src/main/scala/org/virtuslab/inkuire/engine/http/http/Templates.scala
+++ engineHttp/src/main/scala/org/virtuslab/inkuire/engine/http/http/Templates.scala
@@ -65,7 +65,7 @@ object Templates {
         ),
         div(id := "examples")(
           h2("Examples"),
-          generateExamples("Set[Long].(Long) => Boolean", "BigDecimal.() => Byte", "Seq[Int].() => Int", "[A, B] (A, B) => A", "(Int, Long) => Int")
+          generateExamples("Seq[Int].((Int) => Long) => Seq[Long]", "Set[Long].(Long) => Boolean", "BigDecimal.() => Byte", "Seq[Int].() => Int", "[A, B] (A, B) => A", "(Int, Long) => Int", "Seq[Int].((Int) => Seq[Long]) => Seq[Long]")
         )
       )
     ).toString()
