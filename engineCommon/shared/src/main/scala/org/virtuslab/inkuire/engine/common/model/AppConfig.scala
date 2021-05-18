835a8d15ee2465fde38844deaaec2f2cafde024c
Kacper Korban <kacper.f.korban@gmail.com>
2021-05-18 23:45:22 +0200

    Cleanup config

--- engineCommon/shared/src/main/scala/org/virtuslab/inkuire/engine/common/model/AppConfig.scala
+++ engineCommon/shared/src/main/scala/org/virtuslab/inkuire/engine/common/model/AppConfig.scala
@@ -4,21 +4,17 @@ import org.virtuslab.inkuire.engine.common.utils.syntax._
 import cats.implicits._
 
 case class AppConfig(
-  address:            Address,
-  port:               Port,
-  dbPaths:            Seq[DbPath],
-  ancestryGraphPaths: Seq[AncestryGraphPath],
-  inkuirePaths:       Seq[InkuirePath]
+  address:      Address,
+  port:         Port,
+  inkuirePaths: Seq[InkuirePath]
 )
 
 object AppConfig {
   def create(args: List[AppParam]): Either[String, AppConfig] = {
     val address            = args.collectFirst { case a: Address => a }.toRight(noConfigFoundString("address"))
     val port               = args.collectFirst { case p: Port => p }.toRight(noConfigFoundString("port"))
-    val dbPaths            = Right(args.collect { case a: DbPath => a })
-    val ancestryGraphPaths = Right(args.collect { case p: AncestryGraphPath => p })
     val inkuirePaths       = Right(args.collect { case i: InkuirePath => i })
-    (address, port, dbPaths, ancestryGraphPaths, inkuirePaths).mapN(AppConfig.apply)
+    (address, port, inkuirePaths).mapN(AppConfig.apply)
   }
 
   private def noConfigFoundString(paramName: String) =
@@ -28,16 +24,12 @@ object AppConfig {
 trait AppParam extends Any
 case class Address(address: String) extends AnyVal with AppParam
 case class Port(port: Int) extends AnyVal with AppParam
-case class DbPath(path: String) extends AnyVal with AppParam
-case class AncestryGraphPath(path: String) extends AnyVal with AppParam
 case class InkuirePath(path: String) extends AnyVal with AppParam
 
 object AppParam {
   def parseCliOption(opt: String, v: String): Either[String, AppParam] =
     opt match {
-      case "-d" | "--database" => DbPath(v).right
-      case "-a" | "--ancestry" => AncestryGraphPath(v).right
-      case "--address"         => Address(v).right
+      case "-a" | "--address"  => Address(v).right
       case "-p" | "--port"     => Port(v.toInt).right
       case "-i" | "--inkuire"  => InkuirePath(v).right
       case _                   => s"Wrong option $opt".left
