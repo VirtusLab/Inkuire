35c723dede167530eff26ebcac2800062fc88330
Kacper Korban <kacper.f.korban@gmail.com>
2021-05-28 18:19:38 +0200

    Add Monoid instance to InkuireDb

--- engineCommon/shared/src/main/scala/org/virtuslab/inkuire/engine/common/model/InkuireDb.scala
+++ engineCommon/shared/src/main/scala/org/virtuslab/inkuire/engine/common/model/InkuireDb.scala
@@ -6,8 +6,21 @@ import io.circe.parser._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import com.softwaremill.quicklens._
+import cats.kernel.Monoid
 
 case class InkuireDb(
   functions: Seq[ExternalSignature],
   types:     Map[ITID, (Type, Seq[Type])]
 )
+
+object InkuireDb {
+  implicit val inkuireDbMonoid = new Monoid[InkuireDb] {
+    override def combine(x: InkuireDb, y: InkuireDb): InkuireDb =
+      InkuireDb(
+        functions = (x.functions ++ y.functions).distinct,
+        types = x.types ++ y.types
+      )
+
+    override def empty: InkuireDb = InkuireDb(Seq.empty, Map.empty)
+  }
+}
