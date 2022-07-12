fa79a9542c8b68df1eb7886bfc0ed533c2333ec1
Kacper Korban <kacper.f.korban@gmail.com>
2022-07-12 23:17:14 +0200

    Add deserialization tests

---
+++ engine/src/test/scala/org/virtuslab/inkuire/engine/DeserializationTest.scala
@@ -0,0 +1,29 @@
+package org.virtuslab.inkuire.engine
+
+import org.virtuslab.inkuire.engine.impl.service.EngineModelSerializers
+import scala.io.Source
+
+class DeserializationTest extends munit.FunSuite {
+
+  def testDeserialization(version: String)(implicit loc: munit.Location): Unit = {
+    test(s"Read ${version} stdlib") {
+      val filePath: String = s"./engine/src/test/resources/stdlib-${version}.json"
+      val source = Source.fromFile(filePath)
+      val contents = source.mkString
+      source.close()
+      val res = EngineModelSerializers.deserialize(contents)
+      assert(res.isRight)
+    }
+  }
+
+  testDeserialization("3.1.3")
+
+  testDeserialization("3.1.2")
+
+  testDeserialization("3.1.1")
+
+  testDeserialization("3.1.0")
+
+  testDeserialization("3.0.2")
+
+}
